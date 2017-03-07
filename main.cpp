e4944e388fd3f7f4c0b3e49c50999cd42bf14b54
exchange321 <chuen_hong@yahoo.com>
2017-03-07 17:25:08 +1000

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -15,46 +15,6 @@ void main(int argc, char *argv[]) {
 		token = yylex();
 		printf("Next Token: ");
 		switch (token) {
-			case REGEX_START: {
-				printf("REGEX_START (%s)\n", yylval.regexStart);
-				break;
-			}
-			case REGEX_END: {
-				printf("REGEX_END (%s)\n", yylval.regexEnd);
-				break;
-			}
-			case REGEX_BODY: {
-				printf("REGEX_CHARS (%s)\n", yylval.regexBody);
-				break;
-			}
-			case REGEX_CLASS_START: {
-				printf("REGEX_CLASS_START (%s)\n", yylval.regexClassStart);
-				break;
-			}
-			case REGEX_CLASS_BODY: {
-				printf("REGEX_CLASS_CHARS (%s)\n", yylval.regexClassBody);
-				break;
-			}
-			case REGEX_CLASS_END: {
-				printf("REGEX_CLASS_END (%s)\n", yylval.regexClassEnd);
-				break;
-			}
-			case REGEX_BACKSLASH_SEQ: {
-				printf("REGEX_BACKSLASH_SEQ (%s)\n", yylval.regexBackSeq);
-				break;
-			}
-			case REGEX_FLAG: {
-				printf("REGEX_FLAG (%s)\n", yylval.regexFlag);
-				break;
-			}
-			case IDENT: {
-				printf("IDENT (%s)\n", yylval.ident);
-				break;
-			}
-			case CONST: {
-				printf("CONST\n");
-				break;
-			}
 			case 0: {
 				printf("EOF\n");
 				break;
